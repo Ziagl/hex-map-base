a9b3cf45634990ada7a417bfdf646957175e7a46
Ziagl <office@ziegelwanger-edv.at>
2024-10-31 14:13:14 +0100

    version 0.1.10

---
+++ HexMapBase/Models/WeightedCubeCoordinates.cs
@@ -0,0 +1,10 @@
+﻿namespace com.hexagonsimulations.Geometry.Hex.Models;
+
+/// <summary>
+/// Weighted coordinates for path finding and movement.
+/// </summary>
+public record WeightedCubeCoordinates
+{
+    public CubeCoordinates Coordinates;
+    public int Cost;
+}
