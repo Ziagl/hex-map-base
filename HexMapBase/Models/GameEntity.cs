5edd1444519c272ed92d494a2f7f84d319bfd50d
Ziagl <office@ziegelwanger-edv.at>
2025-03-14 10:18:44 +0100

    version 0.3.2

--- HexMapBase/Models/GameEntity.cs
+++ HexMapBase/Models/GameEntity.cs
@@ -1,10 +1,10 @@
 ﻿namespace com.hexagonsimulations.Geometry.Hex.Interfaces;
 
 /// <summary>
-/// This is the base interface for all game objects that can be placed on the map.
+/// This is the base model for all game objects that can be placed on the map.
 /// They belong to a specific player and it has a health value that describes its life status.
 /// </summary>
-public interface IGameEntity
+public abstract class GameEntity
 {
     public int Id { get; set; }         // id of the entity (unique only in its scope)
     public int Player { get; set; }     // id of player who owns the entity
