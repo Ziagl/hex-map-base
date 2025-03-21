fece2c958aea2af2ae96dd4bb84d191f25a27d2d
Ziagl <office@ziegelwanger-edv.at>
2025-03-21 15:37:48 +0100

    version 0.3.3

---
+++ HexMapBase/Interfaces/IBaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace com.hexagonsimulations.Geometry.Hex.Interfaces;
+
+/// <summary>
+/// This is the base interface for all game objects that can be placed on the map.
+/// They belong to a specific player and are described by an uniqu id.
+/// </summary>
+public interface IBaseEntity
+{
+    public int Id { get; set; }         // id of the entity (unique only in its scope)
+    public int Player { get; set; }     // id of player who owns the entity
+}
---
+++ HexMapBase/Interfaces/ICombatEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace com.hexagonsimulations.Geometry.Hex.Interfaces;
+
+/// <summary>
+/// This is the interface for all game objects that can e involved into combat.
+/// They have values for attack and defence and health.
+/// </summary>
+public interface ICombatEntity
+{
+    // health
+    public int Health { get; set; }     // current health of the entity
+    public int MaxHealth { get; set; }  // maximum health of the entity
+    // combat
+    public int WeaponType { get; set; } // type of weapon/combat of this unit (infantry, cavalry, ...)
+    public int Attack { get; set; }     // attack points (damage in fight)
+    public int RangedAttack { get; set; } // ranged attack points (damage of airstrike)
+    public int Defense { get; set; }    // defence points (how much damage is reduced)
+    public int Range { get; set; }      // attack range (how far can this unit attack)
+}
